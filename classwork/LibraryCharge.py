fb9c390eb9441f134e04674f4741112250698349
Choko10 <144367782+Choko10@users.noreply.github.com>
2024-11-01 07:48:19 +0100

    Loop Assignment

---
+++ classwork/LibraryCharge.py
@@ -0,0 +1,13 @@
+LibraryCharge= int(input("Enter number of late days :"))
+if(LibraryCharge <= 5):
+	print("Your fine is 50 paise");
+	elsif
+	(LibraryCharge <= 10)
+	print("Your fine is 1 rupees");
+	elsif
+	(LibraryCharge <= 30) 
+	print("Your fine is 5 rupees")
+	elsif
+	(LibraryCharge >= 31)
+	print("Your membership is cancelled")
+		
