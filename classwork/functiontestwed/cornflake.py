616126fbb4ce6a8637c9aa8de27226bdeff18db9
Choko10 <144367782+Choko10@users.noreply.github.com>
2024-11-13 17:28:06 +0100

    Function classwork

---
+++ classwork/functiontestwed/cornflake.py
@@ -0,0 +1,27 @@
+import math
+def divide_or_square(number):
+	if type(number)in ['str']:
+		return "invalid"
+	if number == -10:
+		return"valid input"
+	if number % 5 != 0:
+		number % 5 
+		return 4 
+	if number % 5 == 0:
+		round(math.sqrt(10),2)
+		return 3.16
+
+#anotherdeclaration#
+
+
+
+
+def future_investment_amount(number1,number2,number3):
+	return(1,2,3)
+		
+	
+
+
+
+
+
---
+++ classwork/functiontestwed/testcornflake.py
@@ -0,0 +1,67 @@
+import unittest
+import cornflake
+import math
+
+
+class TestDivideSquareFunction(unittest.TestCase):
+
+	def test_that_function_corn_flake_exists(self):
+		cornflake.divide_or_square(10)
+
+	def test_that_function_corn_flake_doesnt_take_string(self):
+		self.assertRaises(TypeError,cornflake.divide_or_square, "Edwin")
+
+	def test_that_function_corn_flake_doesnt_take_negetive_input(self):
+		actual = cornflake.divide_or_square(-10)
+		expected = "valid input" 
+		self.assertEqual(actual,expected)
+
+	def test_that_function_doesnt_take_invalid_character(self):
+		self.assertRaises(TypeError,cornflake.divide_or_square, "@")
+
+	def test_that_function_corn_flake_if_mod_number_not_equal_to_zero(self):
+		actual = cornflake.divide_or_square(14)
+		expected = 4
+
+	def test_that_function_corn_flake_mod_square_rounds_to_two_decimal(self):
+		actual = cornflake.divide_or_square(round(math.sqrt(10),2))
+		expected = 3.16
+		
+
+
+#another test for investment amount#
+
+
+
+
+class TestFutureInvestmentAmount(unittest.TestCase):
+	def test_that_function_corn_flake_exists(self):
+		cornflake.future_investment_amount(1,2,3)
+		
+	def test_that_function_corn_flakes_holds_three_arguments(self):
+		self.assertEqual(missingargument,cornflake.future_investment_amount,1,2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == ("__main__"):
+	unittest.main()
