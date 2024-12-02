d3e61599ac79c7f393c2b85984af92484fd324a5
Choko10 <144367782+Choko10@users.noreply.github.com>
2024-12-02 14:46:51 +0100

    Second task phase gate

--- phaseGateOne/TaskOne.py
+++ phaseGateOne/TaskOne.py
@@ -3,7 +3,7 @@ b = 25
 
 sum = a+b
 
-userInput = int(input("Enter the sum of both integers  :"))
+userInput = int(input("Enter the sum of both integers : "))
 if(userInput == sum):
 	print(True)
 else:
---
+++ phaseGateOne/TaskTwo.py
@@ -0,0 +1,7 @@
+userInput = input("Enter an inter between 0 and 1000 : ")
+indexcounter= 0
+for index in userInput:
+	indexcounter+=index
+
+print(indexcounter)
+			
