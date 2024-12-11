27e4f63b235065bb793a3af7246a36f08e7640c1
Choko10 <144367782+Choko10@users.noreply.github.com>
2024-12-11 21:32:46 +0100

    Test commit

--- assignment/SnacksAssignment/test_CeaserCryptograpy.py
+++ assignment/SnacksAssignment/test_CeaserCryptograpy.py
@@ -10,3 +10,13 @@ class TestCaeserCryptographyFunction(unittest.TestCase):
 		actual = ceaser_CryptographyEncrypted("Edwin", 3)
 		expected =" Hgzlq"
 		self.assertEqual(actual, expected)
+
+
+class TestCaeserCryptographyDecryptFunction(unittest.TestCase):
+	def test_that_caeser_cryptography_decrypted_exist(self):
+		ceaser_CryptograpyDecrypted("Edwin", 3)
+
+	def test_that_caeser_cryptography_decrypted_returns_correct_value(self):
+		actual = ceaser_CryptograpyDecrypted("Hgzlq", 3)
+		expected = " Edwin"
+		self.assertEqual(actual, expected)
