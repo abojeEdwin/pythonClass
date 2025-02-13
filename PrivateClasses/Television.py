2a24ea76e882cdd6ea6df8866623e9bb048a5fb3
Edwin Aboje <abojeedwin@gmail.com>
2025-02-13 20:40:58 +0100

    Television task

--- PrivateClasses/Television.py
+++ PrivateClasses/Television.py
@@ -1,55 +1,67 @@
 class Tv:
 
     def __init__(self) :
-        self.previousVolume = 0
-        self.volumeLevel = self.previousVolume
-        self.channelLevel = 0
-        self.isOn = False
+        self.__previousVolume = 0
+        self.__volumeLevel = self.__previousVolume
+        self.__channelLevel = 0
+        self.__isOn = False
 
     def is_On_status(self):
-        return self.isOn
+        return self.__isOn
+
 
     def getvolumeLevel(self):
-        return self.volumeLevel
+        return self.__volumeLevel
 
     def setvolumeLevel(self,volume):
-        self.volumeLevel = volume
+        self.__volumeLevel = volume
 
     def getchannelLevel(self):
-        return self.channelLevel
+        return self.__channelLevel
 
     def channelLevel(self,channel):
-        self.channelLevel = channel
+        self.__channelLevel = channel
 
     def turnOn(self):
-         self.isOn = True
+         self.__isOn = True
 
     def turnOff(self):
-         self.isOn = False
+         self.__isOn = False
 
     def increaseVolume(self):
-        if self.isOn:
-            self.volumeLevel +=1
+        if self.__isOn and self.__volumeLevel < 30:
+            self.__volumeLevel +=1
 
     def decreaseVolume(self):
-        if self.isOn:
-            self.volumeLevel -= 1
+       if self.__isOn:
+            self.__volumeLevel -= 1
 
     def channelDown(self):
-         self.channelLevel -= 1
+         self.__channelLevel -= 1
 
 
     def channelUp(self):
-        if self.channelLevel >= 0:
-            self.channelLevel += 1
+        if not self.__isOn:
+            self.__channelLevel = 0
+
+        if self.__isOn:
+           self.__channelLevel += 1
 
 
     def getChannel(self):
-        return self._channelLevel
-
+        return self.__channelLevel
 
     def setChannel(self, value):
-        self._channelLevel = value
+        if self.__isOn:
+            self.__channelLevel = value
+
+    def mute(self):
+        self.__previousVolume = self.__volumeLevel
+        self.__volumeLevel = 0
+
+    def unmute(self):
+        self.__volumeLevel = self.__previousVolume
+
 
 
 
--- PrivateClasses/tvTest.py
+++ PrivateClasses/tvTest.py
@@ -6,11 +6,17 @@ class MyTestCase(unittest.TestCase):
     def setUp(self):
         self.tv = Tv()
 
-    def test_that_tv_isOn(self):
+    def test_that_tv_is_off(self):
+        self.assertFalse(self.tv.is_On_status())
+
+    def test_that_tv_can_come_on(self):
+        self.assertFalse(self.tv.is_On_status())
         self.tv.turnOn()
         self.assertTrue(self.tv.is_On_status())
 
-    def test_that_tv_isOff(self):
+    def test_that_tv_can_go_off(self):
+        self.tv.turnOn()
+        self.assertTrue(self.tv.is_On_status())
         self.tv.turnOff()
         self.assertFalse(self.tv.is_On_status())
 
@@ -32,29 +38,66 @@ class MyTestCase(unittest.TestCase):
         self.tv.increaseVolume()
         self.tv.increaseVolume()
         self.tv.decreaseVolume()
-        self.assertEqual(2, self.tv.getvolumeLevel())
-
-    def test_that_tv_can_change_channel_down(self):
-        self.tv.turnOff()
-        self.tv.channelDown()
-        self.tv.channelDown()
-        self.tv.channelDown()
-        self.tv.channelDown()
-        self.assertEqual(0,self.tv.getchannelLevel() )
+        self.tv.decreaseVolume()
+        self.assertEqual(1, self.tv.getvolumeLevel())
 
     def test_that_tv_can_change_channel_up(self):
+        self.tv.turnOn()
+        self.tv.channelUp()
+        self.tv.channelUp()
+        self.tv.channelUp()
+        self.tv.channelUp()
+        self.assertEqual(4,self.tv.getchannelLevel() )
+
+    def test_that_tv_can_change_channel_down(self):
+        self.tv.turnOn()
         self.tv.channelUp()
         self.tv.channelUp()
         self.tv.channelUp()
         self.tv.channelDown()
-        self.assertEqual(2, self.tv.getchannelLevel())
+        self.tv.channelDown()
+        self.assertEqual(1, self.tv.getchannelLevel())
 
     def test_that_tv_can_set_channel(self):
+        self.tv.turnOn()
         self.tv.setChannel(7)
-        actual = self.tv.getChannel()
-        expected = 7
-        self.assertEqual(actual, expected)
+        self.assertEqual(7,self.tv.getChannel())
 
+    def test_that_tv_can_mute(self):
+        self.tv.turnOn()
+        self.tv.increaseVolume()
+        self.tv.increaseVolume()
+        self.tv.increaseVolume()
+        self.assertEqual(3, self.tv.getvolumeLevel())
+        self.tv.mute()
+        self.assertEqual(0,self.tv.getvolumeLevel())
+
+    def test_that_tv_can_unmute(self):
+        self.tv.turnOn()
+        self.tv.increaseVolume()
+        self.tv.increaseVolume()
+        self.assertEqual(2,self.tv.getvolumeLevel())
+        self.tv.mute()
+        self.assertEqual(0,self.tv.getvolumeLevel())
+        self.tv.unmute()
+        self.assertEqual(2,self.tv.getvolumeLevel())
+
+    def test_that_television_does_not_increase_volume_when_off(self):
+        self.tv.increaseVolume()
+        self.tv.increaseVolume()
+        self.assertEqual(0,self.tv.getvolumeLevel())
+
+    def test_that_television_does_not_change_channel_when_off(self):
+        self.assertFalse(self.tv.is_On_status())
+        self.tv.channelUp()
+        self.tv.channelUp()
+        self.assertEqual(0,self.tv.getChannel())
+
+    def test_that_television_does_not_increase_volume_beyond_a_limit(self):
+        self.tv.turnOn()
+        for i in range (200):
+            self.tv.increaseVolume()
+        self.assertEqual(30,self.tv.getvolumeLevel())
 
 
 if __name__ == '__main__':
