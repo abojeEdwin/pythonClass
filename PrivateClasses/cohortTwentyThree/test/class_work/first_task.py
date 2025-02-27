20397ac9f87bba95f74851677f9d43c9e3599f2a
Edwin Aboje <abojeedwin@gmail.com>
2025-02-27 17:42:06 +0100

    Python Task

--- PrivateClasses/cohortTwentyThree/test/class_work/first_task.py
+++ PrivateClasses/cohortTwentyThree/test/class_work/first_task.py
@@ -1,15 +1,13 @@
 def first_task(word):
     new_dict = {}
     for char in word:
-        key = new_dict[char]
-        for char2 in new_dict:
-            if char in new_dict:
-                char+=1
-            else:
-                char = 1
+        if char in new_dict:
+            new_dict[char] += 1
+        else:
+            new_dict[char] = 1
     return new_dict
 
-print(first_task("semicolon.africa"))
+
 
 
 
